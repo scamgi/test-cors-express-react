22854f87ec9f27399b04a250dbea55b490df0c76
scamgi <scampini.giacomo@gmail.com>
2024-04-02 12:17:36 +0200

    post request implemented

--- server/server.js
+++ server/server.js
@@ -6,16 +6,31 @@ let users = require("./db/users.json");
 const PORT = 5000;
 const app = express();
 
+// enable middleware to parse body of Content-type: application/json
+app.use(express.json());
+
 app.get("/api/users", (req, res) => {
   res.send(users);
 });
 
-app.delete('/api/users/:id', (req, res) => {
+app.post("/api/users", (req, res) => {
+  const id = 5;
+  const { name, surname } = req.body;
+
+  const newUser = { id, name, surname };
+  users.push(newUser);
+  writeDataToFile("./db/users.json", users);
+  return res.send(newUser);
+});
+
+app.delete("/api/users/:id", (req, res) => {
   const { id } = req.params;
-  users = users.filter(p => p.id !== id);
-  writeDataToFile('./db/users.json');
+  users = users.filter((p) => p.id.toString() !== id);
+  console.log(users);
+  console.log(id);
+  writeDataToFile("./db/users.json", users);
   return res.send({ message: `User ${id} removed.` });
-})
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port: ${PORT}.`);
